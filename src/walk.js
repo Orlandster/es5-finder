666164789799fba0e17ec2eef17ccf15db806e36
Orlando Wenzinger <orlando.wenzinger@bluewin.ch>
2017-05-04 09:47:54 +0200

    code orlandsterify
    add some line breaks

--- src/walk.js
+++ src/walk.js
@@ -2,15 +2,19 @@ const fs = require('fs');
 const path = require('path');
 
 const walk = (dir, done) => {
-  var results = [];
+  let results = [];
+
   fs.readdir(dir, function(err, list) {
     if (err) return done(err);
-    var pending = list.length;
+    let pending = list.length;
     if (!pending) return done(null, results);
+
     list.forEach(function(file) {
       file = path.resolve(dir, file);
+
       fs.stat(file, function(err, stat) {
         if (stat && stat.isDirectory()) {
+
           walk(file, function(err, res) {
             results = results.concat(res);
             if (!--pending) done(null, results);
